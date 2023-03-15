0cfa9126ec2ec6ea79b2e15f1aaa811542d586ab
Risimon <risimon@mail.ru>
2023-03-15 17:05:14 +0000

    commands to use in Linux file system

---
+++ week3.sh
@@ -0,0 +1,38 @@
+mkdir Videos/Watched
+ls -R Videos
+cd Documents/
+mkdir ProjectX ProjectY
+ls
+mkdir -p Thesis/Chapter1 Thesis/Chapter2 Thesis/Chapter3
+ls
+cd
+ls -R Videos Documents
+cd Videos
+touch blockbuster1.ogg blockbuster2.ogg
+cp blockbuster1.ogg blockbuster3.ogg
+ls -l
+cd ../Documents/
+ls
+touch thesis_chapter1.odf thesis_chapter2.odf 
+cp thesis_chapter1.odf thesis_chapter2.odf Thesis ProjectX 17  ls Thesis ProjectX
+cp /etc/hostname .
+cat hostname 
+cp -r Thesis ProjectX
+ls R ProjectX
+ls -R ProjectX
+touch ProjectX/Thesis/Chapter2/thsis_chapter2.odf
+ls -R ProjectX
+cd ../Documents/
+ls -l thesis*
+mv thesis_chapter2.odf thesis_chapter2_reviewed.odf
+ls -l thesis*
+ls Thesis/Chapter1
+mv thesis_chapter1.odf Thesis/Chapter1
+ls Thesis/Chapter1
+ls -l thesis*
+pwd
+ls -l thesis*
+rm thesis_chapter2_reviewed.odf 
+ls -l thesis*
+rm Thesis/Chapter1
+ls -R Thesis
